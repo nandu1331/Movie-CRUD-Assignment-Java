4dff58a032696fa0dc698e908500f939914c621e
nanda <nandamonsterking@gmail.com>
2025-02-10 22:02:26 +0530

    implemented getActorsWithMultipleMovies method [Input: --] [Output: returns actors who acted in multiple movies]

--- src/main/java/services/ActorService.java
+++ src/main/java/services/ActorService.java
@@ -1,11 +1,29 @@
 package services;
+import Models.Actor;
 import Models.Movie;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ActorService {
     private List<Movie> movies;
 
     public ActorService(List<Movie> movies) {
         this.movies = movies;
+    }
+
+    // Get Actors Who Have Worked in Multiple Movies
+    public void getActorsWithMultipleMovies() {
+        Map<Actor, Long> actorCount = new HashMap<>();
+        for (Movie movie : movies) {
+            if (movie.getActors() != null) {
+                for (Actor actor : movie.getActors()) {
+                    actorCount.put(actor, actorCount.getOrDefault(actor, 0L) + 1);
+                }
+            }
+        }
+        actorCount.entrySet().stream()
+                .filter(entry -> entry.getValue() > 1)
+                .forEach(entry -> System.out.println(entry.getKey().getName() + " - " + entry.getValue() + " movies"));
     }
 }
