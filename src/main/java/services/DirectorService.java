5e81113202af2f7d6712c203de64f2317fe0c3cf
nanda <nandamonsterking@gmail.com>
2025-02-10 22:11:30 +0530

    implemented getDirectorsWithMostMovies method [Input: --] [Output: returns top 5 directors with most movies]

--- src/main/java/services/DirectorService.java
+++ src/main/java/services/DirectorService.java
@@ -1,12 +1,27 @@
 package services;
 
+import Models.Director;
 import Models.Movie;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DirectorService {
     private List<Movie> movies;
 
     public DirectorService(List<Movie> movies) {
         this.movies = movies;
+    }
+
+    // Get Directors with the Most Movies (Top 5)
+    public void getDirectorsWithMostMovies() {
+        Map<Director, Long> directorCount = movies.stream()
+                .filter(m -> m.getDirector() != null)
+                .collect(Collectors.groupingBy(Movie::getDirector, Collectors.counting()));
+
+        directorCount.entrySet().stream()
+                .sorted(Map.Entry.<Director, Long>comparingByValue().reversed())
+                .limit(5)
+                .forEach(entry -> System.out.println(entry.getKey().getName() + " - " + entry.getValue() + " movies"));
     }
 }
