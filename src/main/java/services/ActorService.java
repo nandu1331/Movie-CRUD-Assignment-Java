8d8194264be4caed6d3ace4a220eee0a168643b1
nanda <nandamonsterking@gmail.com>
2025-02-10 22:07:10 +0530

    implemented getMoviesOfYoungestActor method [Input: reference date] [Output: returns movies of the youngest actor as per the reference date]

--- src/main/java/services/ActorService.java
+++ src/main/java/services/ActorService.java
@@ -1,9 +1,9 @@
 package services;
 import Models.Actor;
 import Models.Movie;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.time.LocalDate;
+import java.util.*;
 
 public class ActorService {
     private List<Movie> movies;
@@ -24,6 +24,32 @@ public class ActorService {
         }
         actorCount.entrySet().stream()
                 .filter(entry -> entry.getValue() > 1)
-                .forEach(entry -> System.out.println(entry.getKey().getName() + " - " + entry.getValue() + " movies"));
+                .forEach(entry -> System.out.println(
+                        entry.getKey().getName() + " - " + entry.getValue() + " movies")
+                );
+    }
+
+    // Get the Movies of the Youngest Actor (as of a reference date)
+    public void getMoviesOfYoungestActor(LocalDate referenceDate) {
+        // Gather all unique actors from the movie list
+        Set<Actor> movieActors = new HashSet<>();
+        for (Movie movie : movies) {
+            if (movie.getActors() != null) {
+                movieActors.addAll(movie.getActors());
+            }
+        }
+        Optional<Actor> youngest = movieActors.stream()
+                .min(Comparator.comparingInt(actor -> actor.getPresentAge(referenceDate)));
+        if (youngest.isPresent()) {
+            Actor actor = youngest.get();
+            System.out.println("Youngest Actor: " + actor.getName() + ", Age: "
+                    + actor.getPresentAge(referenceDate));
+            System.out.println("Movies featuring " + actor.getName() + ":");
+            movies.stream()
+                    .filter(m -> m.getActors() != null && m.getActors().contains(actor))
+                    .forEach(System.out::println);
+        } else {
+            System.out.println("No actors found in movies.");
+        }
     }
 }
