2d3d8308ae2d80f7d35c61ea506f8a50a1ff935e
nanda <nandamonsterking@gmail.com>
2025-02-11 09:55:30 +0530

    Created Person Interface to abstract Actor and Director models

--- src/main/java/Models/Actor.java
+++ src/main/java/Models/Actor.java
@@ -1,15 +1,15 @@
 package Models;
-import java.time.LocalDate;
-import java.time.Period;
 
-// Actor class
-public class Actor {
+import java.time.LocalDate;
+import Interfaces.Person;
+
+public class Actor implements Person {
     private String id;
     private String name;
     private LocalDate dateOfBirth;
     private String nationality;
 
-    // Constructor Definition
+    // Constructor
     public Actor(String id, String name, LocalDate dateOfBirth, String nationality) {
         this.id = id;
         this.name = name;
@@ -17,47 +17,32 @@ public class Actor {
         this.nationality = nationality;
     }
 
-    // Setter and Getter Methods
+    // Implemented Methods from Person Interface
+    @Override
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
+    @Override
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    @Override
     public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
+    @Override
     public String getNationality() {
         return nationality;
     }
 
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
+    // Retaining getPresentAge method for clarity
+    public int getPresentAge(LocalDate referenceDate) {
+        return getAge(referenceDate); // Calls the default method in Person
     }
 
-    public int getPresentAge(LocalDate date) {
-        if (dateOfBirth != null) {
-            return Period.between(dateOfBirth, date).getYears();
-        }
-        return 0;
-    }
-
-    // Overrided toString method to print movie data in required format
     @Override
     public String toString() {
         return "Actor: [id=" + id + ", name=" + name + ", Date of birth="
